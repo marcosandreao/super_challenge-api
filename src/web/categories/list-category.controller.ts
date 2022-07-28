f4024c8cac39f660577452b2808d1f7c969185c5
Marcos Oliveira <marcosandreao@gmail.com>
2022-07-28 18:27:34 -0400

    swagger documentation

--- src/web/categories/list-category.controller.ts
+++ src/web/categories/list-category.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ListCategoryUseCase } from 'src/domain/categories/list-category.usecase';
 
+@ApiTags('categories')
 @Controller('categories')
 export class ListCategoryController {
 
